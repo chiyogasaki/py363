5db9f008e9173bf5ba82fe40cac4bdbe52be3521
chamon1120 <chamon1120@users.noreply.github.com>
2019-02-10 13:59:39 +0900

    Add files via upload

---
+++ geo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+	
+from xml.etree.ElementTree import parse
+from urllib.parse import quote  
+import urllib.request, urllib.error
+import csv
+
+data = []
+f = open("test.csv", "r")
+reader = csv.reader(f)
+header = next(reader)
+
+for row in reader:
+    data.append(row)
+
+print(row[2])
+
+print(row)
+
+
+
+def adr2geo(adr):
+    api = "http://www.geocoding.jp/api/?v=1.1&q=%s" % (quote(adr.encode('utf-8')))
+    xml = parse(urllib.request.urlopen(api)).getroot()
+
+    lat = xml.find('coordinate/lat').text
+    lng = xml.find('coordinate/lng').text
+    return (float(lat), float(lng))
+
+print(adr2geo("北九州市若松区鴨生田4−９−２"))
+	
+def main():
+    print(adr2geo('北九州市八幡西区千代ケ崎2-6-7-408'))
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
